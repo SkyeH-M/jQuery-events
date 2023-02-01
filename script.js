b137a6dd21477426187e3bcdc019d90f342a6c8d
SkyeH-M <justskye98@gmail.com>
2023-02-01 16:17:51 +0000

    intial commit

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 $(document).ready(function() {
-    $("#stream1_btn").on("click", function() {
-        $(".stream1").removeClass('highlight_stream');
-        $(".stream2").removeClass('highlight_stream');
-        $(".stream3").removeClass('highlight_stream');
-        $(".stream1").addClass('highlight_stream');
-   });
+//     $("#stream1_btn").on("click", function() {
+//         $(".stream1").removeClass('highlight_stream');
+//         $(".stream2").removeClass('highlight_stream');
+//         $(".stream3").removeClass('highlight_stream');
+//         $(".stream1").addClass('highlight_stream');
+//    });
+     $("#stream1_btn").on(function() {
+          // hide makes element vanish
+     $(".stream1").toggle();
+     $(".stream1").show('slow');
+     $(".stream1").show('medium');
+     $(".stream1").show('fast');
+     $(".stream1").show(1000);
+    });
    $("#stream2_btn").on("click", function() {
         $(".stream1").removeClass('highlight_stream');
         $(".stream2").removeClass('highlight_stream');
